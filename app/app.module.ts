6e61eaefb39c75043c0beba211d6278087df025f
Richard Hibbitts <richard.hibbitts@ingramcontent.com>
2016-09-14 17:15:01 -0400

    Part 5

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,17 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { routing } from './app.routing';
+
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
   imports: [ 
       BrowserModule,
-      FormsModule
+      FormsModule,
+      routing
   ],
   declarations: [ 
-      AppComponent,
-      HeroDetailComponent
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
